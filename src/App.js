e2856fe8a6091b15dc9e0929407d514bf7d0966b
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-11 12:23:42 -0600

    recipecommentform component added with material ui staricons for rating visual and defined in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import RecipeCommentForm from "./RecipeCommentForm/RecipeCommentForm";
+
 
 function App() {
-  return (
-    <h1>Star Rating</h1>
+  const handleCommentSubmit = (newComment) => {
+    console.log("New comment submitted:", newComment);
+    // Add code here to handle the new comment submission, such as adding it to a list of comments or submitting it to a server
+  };
 
+  return (
+    <div>
+      <h1>Star Rating</h1>
+      <RecipeCommentForm onCommentSubmit={handleCommentSubmit} />
+    </div>
   );
 }
 
---
+++ src/RecipeCommentForm/RecipeCommentForm.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Rating,
+  TextField,
+  Snackbar,
+  Alert,
+} from "@mui/material";
+import StarIcon from '@mui/icons-material/Star';
+
+const RecipeCommentForm = ({ onCommentSubmit }) => {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+
+  const handleRatingChange = (event, value) => {
+    setRating(value);
+  };
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newComment = {
+      rating: rating,
+      comment: comment.trim(),
+    };
+    onCommentSubmit(newComment);
+    setRating(0);
+    setComment("");
+    setSubmitSuccess(true);
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSubmitSuccess(false);
+  };
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <form onSubmit={handleSubmit}>
+        <Rating
+          name="rating"
+          value={rating}
+          precision={1}
+          onChange={handleRatingChange}
+          emptyIcon={<StarIcon style={{ opacity: 0.55 }} />}
+        />
+        <TextField
+          id="comment"
+          label="Comment (optional)"
+          multiline
+          rows={4}
+          value={comment}
+          onChange={handleCommentChange}
+          fullWidth
+        />
+        <Box sx={{ mt: 2 }}>
+          <Button type="submit" variant="contained" color="primary">
+            Post Comment
+          </Button>
+        </Box>
+      </form>
+      <Snackbar open={submitSuccess} autoHideDuration={6000} onClose={handleSnackbarClose}>
+        <Alert onClose={handleSnackbarClose} severity="success" sx={{ width: '100%' }}>
+          Comment submitted successfully!
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
+};
+
+export default RecipeCommentForm;
