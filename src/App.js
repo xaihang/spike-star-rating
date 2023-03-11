f835de9168c039d642a03d6995bfa764057ae054
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-03-11 12:30:04 -0600

    added recipecommentlist component - this will display the rating and comments post

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import RecipeCommentForm from "./RecipeCommentForm/RecipeCommentForm";
-
+import RecipeCommentList from "./RecipeCommentList/RecipeCommentList";
+import { useState } from "react";
 
 function App() {
+  const [comments, setComments] = useState([]);
+
   const handleCommentSubmit = (newComment) => {
-    console.log("New comment submitted:", newComment);
-    // Add code here to handle the new comment submission, such as adding it to a list of comments or submitting it to a server
+    setComments([...comments, newComment]);
   };
 
   return (
     <div>
       <h1>Star Rating</h1>
       <RecipeCommentForm onCommentSubmit={handleCommentSubmit} />
+      <RecipeCommentList comments={comments} />
     </div>
   );
 }
